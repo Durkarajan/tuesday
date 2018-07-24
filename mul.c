e9776b54a57329ca11d265cf06609bb3960d482d
Durkarajan <36723396+Durkarajan@users.noreply.github.com>
2018-07-24 12:28:12 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <conio.h>
+void main() 
+{ 
+int i,n,d; 
+clrscr() ; 
+scanf("%d",&n) ; 
+scanf("%d",&d) ; 
+printf("\n",d) ; 
+for(i=1;i<=n;i++) 
+if(i%d==0) 
+printf("%d\t",i) ; 
+getch() ; 
+}
